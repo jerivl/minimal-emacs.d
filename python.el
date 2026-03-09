8c3470708963da3c11471213f888ec0b46080efd
Jeri V.L. <jerivl@pm.me>
2026-03-09 02:18:30 -0700

    python.el added

---
+++ python.el
@@ -0,0 +1,4 @@
+(use-package py-vterm-interaction :ensure t :hook (python-mode . py-vterm-interaction-mode))
+(use-package pydoc :ensure t)
+(use-package ein :ensure t)
+(use-package python-pytest :ensure t)
