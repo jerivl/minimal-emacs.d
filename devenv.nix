8fa33bd3759b467a9e4e51b9861fd2a2da77da17
Jeri V.L. <jerivl@pm.me>
2026-03-10 03:47:39 -0700

    refactoring devenv for 2.0.X

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,17 @@
-{ pkgs, lib, config, inputs, ... }:
+{ self, pkgs, lib, config, inputs, ... }:
 let
-  buildInputs = with pkgs; [
-    stdenv.cc.cc
-    libuv
-    zlib
-  ];
+  #############
+  # Derivations
+  #############
+  
   emacs-majutsu = pkgs.stdenv.mkDerivation {
     name = "majutsu";
   
-    src = pkgs.fetchurl {
-      url = "https://github.com/0WD0/majutsu/archive/refs/tags/v0.6.0.tar.gz";
-      sha256 = "sha256-cIOZJQTuHhgpMwDo1nFvAadmxHtiwK9p9xfUNtRtCgE=";
+    src = pkgs.fetchFromGitHub {
+      owner = "0WD0";
+      repo = "majutsu";
+      rev = "40d0f01d1f538266541b741afb03236064980de8";
+      sha256 = "sha256-Xjd2LidVZqZXbQtAqiKr15q+/3XNNie/TSEPQADnz08=";
     };
   
     buildInputs = with pkgs; [
@@ -19,7 +20,6 @@ let
       emacsPackages.magit
       emacsPackages.with-editor
     ];
-    nativeBuildInputs = buildInputs;
   
     buildPhase = ''
       # This is modified from stdenv buildPhase. foundMakefile is used in stdenv checkPhase.
@@ -45,9 +45,9 @@ let
     pname = "aicommit2";
     version = "v2.4.28";
     src = pkgs.fetchurl {
-        url = "https://github.com/tak-bro/aicommit2/archive/refs/tags/v2.4.28.tar.gz";
-        sha256 = "sha256-WH9ChUDolbIx7w+CynRYJjVH9YWB6T3dantyWRwHfMs=";
-      };
+      url = "https://github.com/tak-bro/aicommit2/archive/refs/tags/v2.4.28.tar.gz";
+      sha256 = "sha256-WH9ChUDolbIx7w+CynRYJjVH9YWB6T3dantyWRwHfMs=";
+    };
   
     pnpmDeps = pkgs.fetchPnpmDeps {
       inherit (finalAttrs) pname version src;
@@ -81,75 +81,117 @@ let
       runHook postInstall
     '';
   });
-  
-  emacs-pre-early-init = '' 
-    (setq user-emacs-directory "${config.git.root}")
-  '';
-  emacs-early-init = builtins.readFile "${config.git.root}/early-init.el";
-  emacs-post-early-init = "";
-  emacs-pre-init = ''
-    (setq parinfer-rust-library "${pkgs.parinfer-rust-emacs}/lib/libparinfer_rust.so")
-  '';
-  emacs-init = builtins.readFile "${config.git.root}/init.el";
-  emacs-post-init = (builtins.readFile "${config.git.root}/post-init.el") + ''
-    (require 'majutsu)
-  '';
-  emacs-config = lib.strings.concatLines [
-    emacs-pre-early-init
-    emacs-early-init
-    emacs-post-early-init
-    emacs-pre-init
-    emacs-init
-    emacs-post-init
-  ];
-  emacs-configured = pkgs.emacsWithPackagesFromUsePackage {
-    config = emacs-config;
-    defaultInitFile = true;
-    package = pkgs.emacs;
-    extraEmacsPackages = epkgs: (with epkgs; [
-      jinx
-      eat
-      vterm
+  ##########
+  # Emacs
+  ##########
+    emacs-bundle = (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages
+    (epkgs: [emacs-majutsu] ++ (with epkgs; [
+      aggressive-indent
+      apheleia
+      buffer-terminator
+      cape
+      catppuccin-theme
+      clipetty
+      consult
+      corfu
+      corfu-terminal 
+      diff-hl 
+      doom-modeline 
+      eat 
+      eglot
+      eglot-booster
+      elisp-refs
+      embark
+      embark-consult
+      envrc 
+      flycheck
+      flyover
+      free-keys 
+      helpful
+      highlight-defined
+      indent-control
+      kind-icon
+      kirigami
+      magit 
+      magit-section 
+      marginalia
+      markdown-mode
+      meow
+      orderless
+      org
+      org-auto-tangle
+      page-break-lines
       parinfer-rust-mode
-      # unsure if I should move lsp bridge here
-    ])
-    ++ [ emacs-majutsu ];
+      puni
+      show-eol 
+      stripspace
+      tempel
+      tempel-collection 
+      treemacs
+      treesit-auto
+      undo-fu
+      undo-fu-session
+      vertico
+      vundo 
+      which-key
+      with-editor       
+    ]));
+
+  emacs-configured = pkgs.writeShellApplication {
+    name = "emacs";
+    runtimeInputs = [ emacs-bundle ];
+    text = ''
+      emacs -Q --load ${config}/rolling/config.el "$@"
+    '';
   };
   
+  url = https://raw.githubusercontent.com/jerivl/minimal-emacs.d;
+  name = "config.el";
+  config = (pkgs.writeTextDir "/rolling/${name}")
+    ( builtins.readFile (
+    pkgs.concatTextFile {
+      inherit name;
+      files = [
+        (builtins.fetchurl { url = "${url}/refs/heads/update/lexical-binding"; })
+        (pkgs.writeText "pre-pre-early-init.el" ''
+          (setq user-emacs-directory "${self.outPath}")
+          (setq parinfer-rust-library "${pkgs.parinfer-rust-emacs}/lib/libparinfer_rust.so")
+        '')
+        #(builtins.fetchurl { url = "${url}/refs/heads/main/pre-early-init.el"; })
+        (builtins.fetchurl { url = "${url}/refs/heads/update/early-init.el";})
+        #(builtins.fetchurl { url = "${url}/refs/heads/main/post-early-init.el"; })
+        #(builtins.fetchurl { url = "${url}/refs/heads/main/pre-init.el"; })
+        (builtins.fetchurl { url = "${url}/refs/heads/update/init.el";})
+        (builtins.fetchurl { url = "${url}/refs/heads/update/post-init.el";})
+        (pkgs.writeText "post-post-init.el" ''
+          (require 'majutsu)
+        '')    
+      ];
+    })
+  );
+
 in 
 {
-  env.GREET = "minimal-emacs.d";
 
-  packages = with pkgs; [
-    git
-    jq
-    jujutsu
-    
-    cocogitto
-    koji
-    aicommit2
-    emacs-lsp-booster
-    parinfer-rust-emacs
-    ty
-  ]
-  ++ [ emacs-configured ]
   
-  ;  
-  git-hooks = {
-    enable = true;
-    hooks = {
-    };
-  };
-  treefmt = {
-    enable = true;
-    config.programs = {
-    };
-  };
+  packages = [ emacs-configured ] ++ (with pkgs; [
+    aicommit2
+    cocogitto
+    emacs-lsp-booster
+    jq
+    #jujutsu
+    koji
+    parinfer-rust-emacs
+    git
+  ]) ++ (with (import inputs.nixpkgs-unstable { }); [ jujutsu ]);
+  
   overlays = [
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
     }))
-  ];    
+  ];
+
+  env.GREET = "minimal-emacs.decoupled";
   scripts.hello.exec = ''
     echo Hello from $GREET
   '';
@@ -165,21 +207,10 @@ in
       fi
     fi
   '';
-  
 
-  # https://devenv.sh/basics/
   enterShell = ''
     first-setup-check
     hello
   '';
 
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
