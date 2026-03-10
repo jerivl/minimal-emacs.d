4315d8f37b2961a77069e02189552bc1b3b17c9a
Jeri V.L. <jerivl@pm.me>
2026-03-09 19:22:25 -0700

    Add emacs-pet

--- python.el
+++ python.el
@@ -2,3 +2,6 @@
 (use-package pydoc :ensure t)
 (use-package ein :ensure t)
 (use-package python-pytest :ensure t)
+(use-package pet :ensure t
+  :config
+  (add-hook 'python-base-mode-hook 'pet-mode -10))
